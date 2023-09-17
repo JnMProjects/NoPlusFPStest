fd9990e99ae6b53fc93bc0fed9b5d2e68a01550b
derjacklive <71508487+derjacklive@users.noreply.github.com>
2023-09-17 16:49:26 +0200

    Map funktioniert mit button #2

--- Jacks Fps Test/Assets/Scripts/InputManager.cs
+++ Jacks Fps Test/Assets/Scripts/InputManager.cs
@@ -6,18 +6,32 @@ using UnityEngine.InputSystem;
 public class InputManager : MonoBehaviour
 {
     private PlayerInput playerInput;
-    private PlayerInput.OnFootActions onFoot;
+    public PlayerInput.OnFootActions onFoot;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
     // Start is called before the first frame update
     void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+        onFoot.Crouch.performed += ctx => motor.Crouch();
+        onFoot.Sprint.performed += ctx => motor.Sprint();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // tell motor to use vector2 from calculation
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
