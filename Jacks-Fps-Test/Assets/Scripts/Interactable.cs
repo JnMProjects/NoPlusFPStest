59f8734dcdd86e7b389816011d26cce2aa8fa0cf
derjacklive <71508487+derjacklive@users.noreply.github.com>
2023-10-16 22:46:35 +0200

    Update: Interactions now Easy to use
    New Scripts to Easaly Use Interactions with Events (Custom Editor) fixes #3

--- Jacks-Fps-Test/Assets/Scripts/Interactable.cs
+++ Jacks-Fps-Test/Assets/Scripts/Interactable.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool UseEvents;
     public string promptMessage;
 
 
     public void BaseInteract()
     {
+        if (UseEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
     
